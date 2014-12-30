ca5e8d0d89e05e852f465a19f25902c7f19d3587
Iain <public@soulflyer.co.uk>
2014-12-30 09:10:55 +0700

    Initial Commit

---
+++ MidiTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MidiTest
+//
+//  Created by Iain Wood on 30/12/2014.
+//  Copyright (c) 2014 soulflyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MidiTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MidiTest
+//
+//  Created by Iain Wood on 30/12/2014.
+//  Copyright (c) 2014 soulflyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
